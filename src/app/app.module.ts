6b3fe0f754c271b6e0e20adbc2e618a247225dad
AnishaWadhwani <wanisha2013@gmail.com>
2023-06-28 01:36:56 -0400

    Add components to homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskCardComponent } from './task-card/task-card.component';
+import { AddNewTaskComponent } from './add-new-task/add-new-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskCardComponent,
+    AddNewTaskComponent
   ],
   imports: [
     BrowserModule,
