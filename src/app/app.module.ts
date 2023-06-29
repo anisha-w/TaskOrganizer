f529af41855170bd0dfda8e6cb0efdb99236ea58
AnishaWadhwani <wanisha2013@gmail.com>
2023-06-29 19:07:16 -0400

    installing cdkdragdrop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,18 +10,21 @@ import { TaskCardComponent } from './task-card/task-card.component';
 import { AddNewTaskComponent } from './add-new-task/add-new-task.component';
 
 import { TaskDetail } from './task-detail';
+import { CalenderViewComponent } from './calender-view/calender-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskListComponent,
     TaskCardComponent,
-    AddNewTaskComponent
+    AddNewTaskComponent,
+    CalenderViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
