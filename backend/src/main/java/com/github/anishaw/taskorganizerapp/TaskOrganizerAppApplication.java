a437b428ffe58d76aaea48715b8d0af5de3d0ff8
AnishaWadhwani <wanisha2013@gmail.com>
2023-08-10 13:32:21 -0400

    Integrating Springboot

---
+++ backend/src/main/java/com/github/anishaw/taskorganizerapp/TaskOrganizerAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.anishaw.taskorganizerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskOrganizerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskOrganizerAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/anishaw/taskorganizerapp/TaskOrganizerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.anishaw.taskorganizerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskOrganizerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
