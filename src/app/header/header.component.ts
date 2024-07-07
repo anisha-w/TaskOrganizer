cfd7616b5fe1b44a894c2f49123558ca94c45118
AnishaWadhwani <wanisha2013@gmail.com>
2024-07-07 18:09:10 -0400

    Updated Header for Export btn

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IcsService } from '../ics.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private icsService: IcsService) {}
 
   ngOnInit(): void {
   }
 
+  downloadICS(){
+    const startDate = new Date(2024, 6, 10, 6, 30);
+    const endDate = new Date(2024, 6, 10, 7, 30); 
+    const eventDetails = [{
+      start: startDate,
+      end : endDate,
+      title: 'Sample Event',
+      description: 'This is a sample event description',
+      location: 'Online',
+      url: 'https://example.com',
+      organizer: { name: 'Organizer Name', email: 'organizer@example.com' },
+      attendees: [
+        { name: 'Attendee One', email: 'attendee1@example.com' },
+        { name: 'Attendee Two', email: 'attendee2@example.com' }
+      ]
+    }];
+    this.icsService.generateICSFile(eventDetails);
+  }
 }
